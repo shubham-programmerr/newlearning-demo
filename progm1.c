dbe6cb34cd5c1d69f856bc331042375a4d75cf36
shubham-programmer <dadhichshubham32@gmail.com>
2024-11-15 20:12:58 +0530

    adding of two number

---
+++ progm1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int num1,num2,num;
+    printf("enter the number 1:\n");
+    scanf("%d",&num1);
+    printf("enter the number2:\n");
+    scanf("%d",&num2);
+    num=num1+num2;
+    printf("sum of %d and %d = %d",num1,num2,num);
+}
