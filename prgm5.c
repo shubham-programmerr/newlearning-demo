6a21d24a1b7baffbacfd7411adfa8f12f3186a01
shubham-programmer <dadhichshubham32@gmail.com>
2024-11-22 21:31:27 +0530

    removed the error from the program

--- prgm5.c
+++ prgm5.c
@@ -3,9 +3,9 @@
 #define max 10
 void main()
 {
-    int arr[max], top=-1,size,value,i;
-    printf("the creation of stack of stack with insertion\n");
-    printf("----------------------------------------------\n");
+    int arr[max], top=-1,size,value,i,pop,num;
+    printf("the creation of stack of with insertion\n");
+    printf("---------------------------------------\n");
     for(i=0;i<=max;i++)
     {
         if(top==max-1)
@@ -22,6 +22,22 @@ void main()
     {
         printf("\n%d\t",arr[i]);
     }
-
-
+    printf("\nthe pop from stack of written\n");
+    printf("-----------------------------\n");
+    printf("enter the number of steps you want to delete\n");
+    scanf("%d",&num);
+    if(num>max)
+    {
+        printf("the given step is invalid\n");
+    }
+    else
+    {
+        for(i=0;i<=num-1;i++)
+        {
+            pop=arr[top];
+            printf("the popped element from stack:%d\n",pop);
+            top--;
+        }
+    }
+    printf("end of program..............");
 }
