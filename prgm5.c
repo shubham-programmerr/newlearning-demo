0372d20aaff8a0e77fe7c15031072858e37b2055
shubham-programmer <dadhichshubham32@gmail.com>
2024-11-22 22:01:33 +0530

    upgrade the program5.c

--- prgm5.c
+++ prgm5.c
@@ -6,29 +6,39 @@ void main()
     int arr[max], top=-1,size,value,i,pop,num;
     printf("the creation of stack of with insertion\n");
     printf("---------------------------------------\n");
-    for(i=0;i<=max;i++)
+    printf("entee the size of the stack you want:");
+    scanf("%d",&size);
+    if(size>max)                                                      //push of elements in stack
     {
-        if(top==max-1)
-        {
-            printf("the stack is overflow");
-            break;
-        }
-        top++;
-        printf("\nenter the value of the array:");
-        scanf("%d",&arr[top]);
-        
+        printf("the size is more the max of stack");
+        return;
     }
-    for(i=top;i>=0;i--)
+    else
+    {
+        for(i=0;i<=size-1;i++)
+        {
+            if(top==max-1)
+            {
+                printf("the stack is overflow");
+                break;
+            }
+            top++;
+            printf("\nenter the value of the array:");
+            scanf("%d",&arr[top]);
+        }
+    }
+    for(i=top;i>=0;i--)                                               //display of elments in stack
     {
         printf("\n%d\t",arr[i]);
     }
-    printf("\nthe pop from stack of written\n");
+    printf("%d",top);
+    printf("\nthe pop from stack of written\n");                      //pop of elements in stack
     printf("-----------------------------\n");
-    printf("enter the number of steps you want to delete\n");
+    printf("enter the number of steps you want to delete\n");             
     scanf("%d",&num);
-    if(num>max)
+    if(num>top)
     {
-        printf("the given step is invalid\n");
+        printf("the given steps is invalid\n");
     }
     else
     {
