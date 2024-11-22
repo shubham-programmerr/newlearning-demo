bfa874e7f413437c7726ef8878435e44e878b290
shubham-programmer <dadhichshubham32@gmail.com>
2024-11-22 22:19:49 +0530

    the program 6 file is added

---
+++ prgm6.c
@@ -0,0 +1,49 @@
+void create(struct node *head,int n);
+void display(struct node *head);
+
+void create(struct node *head,int n)
+{
+	struct node *p,*temp;
+	head=NULL;
+	int i ;
+	while(i!=n)
+	{
+		struct node * newnode = (struct node *)malloc(sizeof(struct node*));
+		printf("enter the data here:");
+		scanf("%d",&newnode->data);
+		newnode->nest=NULL;  //new node formation 
+		if(head==NULL)
+		{
+			head=newnode;   //make new node as first or head node 
+		}
+		else
+		{
+			p=head;
+			while(p->nest!=NULL)
+			{
+				p=p->nest;
+			}
+			p->nest=newnode;
+		}
+		i++;	
+	}
+	printf("done of process");
+}
+void display(struct node *head)
+{
+    struct node *dply=head; 
+	while(dply!=NULL)
+	{
+		printf("element %d",dply->data);
+		dply=dply->nest;
+	}
+}
+void main()
+{
+	int n;
+	struct node *head;
+	printf("enter the number of node needed:");
+	scanf("%d",&n);
+	create(head,n);
+	display(head);
+}
