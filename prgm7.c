b049944604359dc23b363c19b568f53a070d8984
shubham-programmer <dadhichshubham32@gmail.com>
2024-12-01 23:05:18 +0530

    updating the program

--- prgm7.c
+++ prgm7.c
@@ -1,6 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct   
+struct node {
+    int data;
+    struct node * next;
+};
+void creation(struct node *head)
+{
+    
+} 
+void insertion(struct node *head){
+    struct node *newnode=(struct node*)malloc(sizeof(struct node*));
+    int value;
+    printf("enter the value number:");
+    scanf("%d",&ptr->data);
+    ptr->next= NULL;
+    if(head==NULL)
+    {
+        head=newnode;
+    }
+    else
+    {
+        
+    }
+}
 void main()
 {
     
--- progm1.c
+++ progm1.c
@@ -1,11 +1,19 @@
 #include<stdio.h>
+struct node {
+    int data;
+    struct node *next;
+};
+void creation_node(struct node *head)
+{ 
+    int value;
+    printf("enter the data for node:");
+    scanf("%d",&value);
+    struct node *newnode = (struct node *)malloc(sizeof(struct node *));
+    newnode->data=value;
+    newnode->next=NULL;
+}
 void main()
 {
-    int num1,num2,num;
-    printf("enter the number 1:\n");
-    scanf("%d",&num1);
-    printf("enter the number2:\n");
-    scanf("%d",&num2);
-    num=num1+num2;
-    printf("sum of %d and %d = %d",num1,num2,num);
+    struct node * head;
+
 }
