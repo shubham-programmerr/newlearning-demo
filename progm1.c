d9c10f01531efeabab49b14758ea5c05dc72801e
shubham-programmer <dadhichshubham32@gmail.com>
2024-12-06 22:49:52 +0530

    updated the progm1.c

--- progm1.c
+++ progm1.c
@@ -7,29 +7,33 @@ struct node {
 void creation_node(struct node *head)
 { 
     struct node *ptr;
-    int value;
-    printf("enter the data for node:");
-    scanf("%d",&value);
+    int value,i,size=3;
     struct node *newnode;
-    newnode= (struct node *)malloc(sizeof(struct node*));
-    newnode->data=value;
-    newnode->next=NULL;
+
     printf("newnode=%d\n",newnode->data);
-    if(head==NULL)
+    for(i=0;i<size;i++)
     {
-        head=newnode;
-        printf("head node = %d\n",head->data);
-    }
-    else
-    {
-        ptr=head;
-        while(ptr->next = NULL)
+        newnode= (struct node *)malloc(sizeof(struct node*));
+        newnode->data=value;
+        newnode->next=NULL;
+        printf("enter the data for node:");
+        scanf("%d",&value);
+        if(head==NULL)
         {
-            ptr= ptr->next;
-           
-        }ptr->next=newnode;
-        printf("insert successfull\n");
+            head=newnode;
+            printf("head node = %d\n",head->data);
+        }
+        else
+        {
+            ptr=head;
+            while(ptr->next = NULL)
+            {
+                ptr= ptr->next;
+            }ptr->next=newnode;
+            printf("insert successfull\n");
     }
+    }
+
     
 }
 void display(struct node * head)
@@ -47,12 +51,7 @@ void main()
 {
     struct node * head;
     int size,i;
-    printf("enter the number of nodes you want in linked list");
-    scanf("%d",&size);
-    for(i=0;i<size;i++)
-    {
-        creation_node(head);
-    }
+    creation_node(head);
     display(head);
 
 }
